b736b0620cb4bec25b025d8d3e641d1042aa64b2
THCG\agbarm2 <mousamohammed.agbaria@medtronic.com>
2023-10-29 16:55:29 +0200

    Add porogram.cs file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace ExampleProject
+{
+
+  
+    class Program
+    {
+        static void Main()
+        {
+            List<int> list = new List<int>() { 1,2,3};
+           
+            IEnumerable<int> list2 = list.Take(10);
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
